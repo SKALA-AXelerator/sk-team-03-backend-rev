d58c6000e04f6c6b08dea89187a60e31857fd7f3
BOOK-1J8KICSOOI\shdhk <shdhkims@naver.com>
2025-06-19 14:03:08 +0900

    chore: 쿠버네티스 자동 배포

--- src/main/java/com/skala03/skala_backend/client/FastApiClient.java
+++ src/main/java/com/skala03/skala_backend/client/FastApiClient.java
@@ -24,7 +24,7 @@ public class FastApiClient {
 
     private WebClient webClient;
 
-    @Value("${fastapi.base-url:https://intervia.skala25a.project.skala-ai.com/ai}")
+    @Value("${fastapi.base-url:https://intervia.skala25a.project.skala-ai.com}")
     private String fastApiBaseUrl;
 
     @Value("${fastapi.api-key:internal-api-key}")
