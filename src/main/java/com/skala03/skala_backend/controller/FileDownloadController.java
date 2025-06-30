6f7912c972829610f29ff2e6196e6e121ce8e9ba
BOOK-1J8KICSOOI\shdhk <shdhkims@naver.com>
2025-06-30 16:53:29 +0900

    refactor: ZIP 파일 다운로드 api 주석 수정

--- src/main/java/com/skala03/skala_backend/controller/FileDownloadController.java
+++ src/main/java/com/skala03/skala_backend/controller/FileDownloadController.java
@@ -1,6 +1,11 @@
 package com.skala03.skala_backend.controller;
 
 import com.skala03.skala_backend.service.FileDownloadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
@@ -13,7 +18,8 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-@RequestMapping("/api/admin/files")
+@RequestMapping("/api/admin")
+@Tag(name = "관리자 API", description = "관리자 기능 - 키워드 관리")
 @RequiredArgsConstructor
 @Slf4j
 public class FileDownloadController {
@@ -23,7 +29,16 @@ public class FileDownloadController {
     /**
      * 특정 직무의 모든 지원자 PDF 파일들을 ZIP으로 다운로드
      */
-    @GetMapping("/download-pdf-zip/{jobRoleId}")
+    @GetMapping("/files/download-pdf-zip/{jobRoleId}")
+    @Operation(summary = "직무별 PDF ZIP 다운로드")
+    @ApiResponse(
+            responseCode = "200",
+            description = "ZIP 바이너리 파일 다운로드",
+            content = @Content(
+                    mediaType = "application/zip",
+                    schema = @Schema(type = "string", format = "binary")
+            )
+    )
     public ResponseEntity<ByteArrayResource> downloadPdfZip(@PathVariable String jobRoleId) {
         log.info("PDF ZIP 다운로드 요청 - 직무 ID: {}", jobRoleId);
 
@@ -53,7 +68,16 @@ public class FileDownloadController {
     /**
      * 특정 직무의 모든 지원자 QNA 파일들을 ZIP으로 다운로드
      */
-    @GetMapping("/download-qna-zip/{jobRoleId}")
+    @GetMapping("/files/download-qna-zip/{jobRoleId}")
+    @Operation(summary = "직무별 QNA ZIP 다운로드")
+    @ApiResponse(
+            responseCode = "200",
+            description = "ZIP 바이너리 파일 다운로드",
+            content = @Content(
+                    mediaType = "application/zip",
+                    schema = @Schema(type = "string", format = "binary")
+            )
+    )
     public ResponseEntity<ByteArrayResource> downloadQnaZip(@PathVariable String jobRoleId) {
         log.info("QNA ZIP 다운로드 요청 - 직무 ID: {}", jobRoleId);
 
